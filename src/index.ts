8a7adf3d7e1043d823ea7efa55a195a0ac12733b
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-13 20:29:03 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let vuelta1: number = Number(prompt("tiempo vuelta uno"));
+let vuelta2: number = Number(prompt("tiempo vuelta dos"));
+let vuelta3: number = Number(prompt("tiempo vuelta tres"));
+let vuelta4: number = Number(prompt("tiempo vuelta cuatro"));
+
+let tiempototal:number;
+
+tiempototal=vuelta1+vuelta2+vuelta3+vuelta4;
+
+console.log("El tiempo total es:" tiempototal);
+
+let promediotiempototal
+
+promediotiempototal =tiempototal/4;
+    
+
+
+console.log("El promedio de tiempo es:" + promediotiempototal);
